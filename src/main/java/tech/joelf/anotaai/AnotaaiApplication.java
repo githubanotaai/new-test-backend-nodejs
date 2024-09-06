41b4d330c740dad70412fcb32d8393e10a2e421e
Joel Filho <joelfilho02812@gmail.com>
2024-09-06 16:09:27 -0300

    chore: enable rabbit

--- src/main/java/tech/joelf/anotaai/AnotaaiApplication.java
+++ src/main/java/tech/joelf/anotaai/AnotaaiApplication.java
@@ -1,8 +1,10 @@
 package tech.joelf.anotaai;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRabbit
 @SpringBootApplication
 public class AnotaaiApplication {
 
