e2b1087c66d29f706fd39ae2623d5233841bc7b8
Joel Filho <joelfilho02812@gmail.com>
2024-09-06 15:03:48 -0300

    chore: init project

---
+++ src/main/java/tech/joelf/anotaai/AnotaaiApplication.java
@@ -0,0 +1,12 @@
+package tech.joelf.anotaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotaaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotaaiApplication.class, args);
+	}
+}
---
+++ src/test/java/tech/joelf/anotaai/AnotaaiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.joelf.anotaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotaaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
