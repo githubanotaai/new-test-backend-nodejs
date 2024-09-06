1c02ab1bc21515683eab976bb8cba88859339a93
Joel Filho <joelfilho02812@gmail.com>
2024-09-06 15:17:24 -0300

    feat: ensure Owner has many categories

---
+++ src/main/java/tech/joelf/anotaai/models/Category.java
@@ -0,0 +1,59 @@
+package tech.joelf.anotaai.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+}
---
+++ src/main/java/tech/joelf/anotaai/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package tech.joelf.anotaai.repositories;
+
+import java.util.Locale.Category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
