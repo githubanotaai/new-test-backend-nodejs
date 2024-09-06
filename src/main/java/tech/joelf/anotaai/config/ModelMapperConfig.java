88bcaf0bdda573b76dd75f469155536ca8fef89e
Joel Filho <joelfilho02812@gmail.com>
2024-09-06 15:23:24 -0300

    chore: add modelmapper

---
+++ src/main/java/tech/joelf/anotaai/config/ModelMapperConfig.java
@@ -0,0 +1,18 @@
+package tech.joelf.anotaai.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        return modelMapper;
+    }
+}
